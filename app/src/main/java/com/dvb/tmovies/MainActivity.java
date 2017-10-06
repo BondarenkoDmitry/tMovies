45a311aced6835bebd13417213f4d09f4fe5cde6
BondarenkoDmitry <demianbondarenko@gmail.com>
2017-10-06 12:11:26 +0100

    Got stuck at "lenght()"

--- app/src/main/java/com/dvb/tmovies/MainActivity.java
+++ app/src/main/java/com/dvb/tmovies/MainActivity.java
@@ -1,11 +1,16 @@
 package com.dvb.tmovies;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -13,28 +18,38 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+
+import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+
+    private PopularMovie[] mPopularMovie;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String apiKey = "?api_key=957c988676c0d274a6d1cc76dd5c8a93";
-//        https://api.themoviedb.org/3/movie/503?api_key=957c988676c0d274a6d1cc76dd5c8a93&language=en-US
+        String apiKey = "?api_key=";
         String siteUrl = "https://api.themoviedb.org/3/movie/";
         String sortBy = "popular";
+        String movie_id = "565";
 
-        final String requestUrl = siteUrl + sortBy + apiKey;
+        final String movieIdRequestUrl = siteUrl + movie_id + apiKey;
+        final String popularMovieRequestUrl = siteUrl + sortBy + apiKey;
 
         if (isNetworkAvailable()) {
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
-                    .url(requestUrl)
+                    .url(siteUrl)
                     .build();
 
             Call call = client.newCall(request);
@@ -46,23 +61,52 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onResponse(Response response) throws IOException {
-                    try{
-                        Log.v(TAG, response.body().string());
-                        if (response.isSuccessful()) {
-                        } else {
-                               alertUserAboutError();
-                            }
-                        } catch (IOException e){
-                        Log.e(TAG, "Exception caught:", e);
-                    }
+                    try {
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
 
+                        if (response.isSuccessful()) {
+                            mPopularMovie = getMovieData(jsonData);
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                    catch (JSONException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
                 }
             });
+        } else {
+            Toast.makeText(this, getString(R.string.network_unavailable_message),
+                    Toast.LENGTH_LONG).show();
         }
+
+        Log.d(TAG, "Main UI code is running!");
+
     }
 
 
 
+    private PopularMovie[] getMovieData(String jsonData) throws JSONException {
+        JSONObject movie = new JSONObject(jsonData);
+        JSONArray movieResults = movie.getJSONArray("results");
+
+        PopularMovie[] movies = new PopularMovie[movieResults.lenght()];
+
+        for (int i = 0; i < movieResults.length(); i++){
+            JSONObject jsonMovie = movieResults.getJSONObject(i);
+            PopularMovie aMovie = new PopularMovie();
+
+            aMovie.setPoster_path(jsonMovie.getString("poster_path"));
+            aMovie.setTitle(jsonMovie.getString("title"));
+
+            movies[i] = aMovie;
+        }
+
+        return movies;
+    }
 
 
     private boolean isNetworkAvailable() {
---
+++ app/src/main/java/com/dvb/tmovies/PopularMovie.java
@@ -0,0 +1,29 @@
+package com.dvb.tmovies;
+
+/**
+ * Created by dmitrybondarenko on 05.10.17.
+ */
+
+public class PopularMovie {
+
+    private String title;
+    private String poster_path;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+}
